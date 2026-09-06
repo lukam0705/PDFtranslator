1f472838b8a7b4b02a9ddcf493185c229d00ad3d
lukam0705 <lukamaricic0705@icloud.com>
2026-09-07 01:03:02 +0200

    Adding Files

--- GUI.py
+++ GUI.py
@@ -1,4 +1,35 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog as fd
+root = Tk()
+root.title('Translator')
+root.resizable(False, False)
+root.geometry('600x460')
 
-print("TEST2")
-#test
+
+def select_file():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+    global filename
+    filename = fd.askopenfilename(
+        title='Prenesi PDF',
+        initialdir='/',
+        filetypes=filetypes)
+
+open_button = ttk.Button(
+    root,
+    text='Prenesi PDF',
+    command=select_file
+)
+
+
+open_button.pack(expand=True)
+
+root.mainloop()
+
+try:
+    print(filename)
+except:
+    print("No Files Selected!")
