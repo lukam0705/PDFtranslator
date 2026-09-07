91af48272ec1033dec21fa907718dfd5a537bc78
lukam0705 <lukamaricic0705@icloud.com>
2026-09-07 12:00:03 +0200

    GUI v:"0.1"

--- GUI.py
+++ GUI.py
@@ -4,32 +4,40 @@ from tkinter import filedialog as fd
 root = Tk()
 root.title('Translator')
 root.resizable(False, False)
-root.geometry('600x460')
+root.geometry('300x200')
 
 
 def select_file():
     filetypes = (
+        ('text files', '*.pdf'),
         ('text files', '*.txt'),
         ('All files', '*.*')
     )
     global filename
     filename = fd.askopenfilename(
-        title='Prenesi PDF',
+        title='Prenesi Tekstualnu Datoteku',
         initialdir='/',
         filetypes=filetypes)
 
 open_button = ttk.Button(
     root,
-    text='Prenesi PDF',
+    text='Prenesi Tekstualnu Datoteku',
     command=select_file
 )
 
+def submited_file():
+    try:
+        print(filename)
+    except:
+        print("No Files Selected!")
+
+send_file = ttk.Button(root,text='Pošalji',command=submited_file)
+
+
+
 
 open_button.pack(expand=True)
+send_file.pack(expand=True)
 
 root.mainloop()
 
-try:
-    print(filename)
-except:
-    print("No Files Selected!")
