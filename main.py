93fc0e7f9a586aa509815201622ca09e7c039567
lukam0705 <lukamaricic0705@icloud.com>
2026-09-08 11:46:18 +0200

    local LLM added

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import GUI
+import localLLM
+
 print("Hello World!")
+
+#   Povezati lokali AI model sa skriptom
+#   Poslje nadograditi sve kako spada
+#   v: 1.0 ce samo raditi sa pure text pdf
+#   sve povezati u main skriptu
+#   uredjivanje isti font odlomci itd... druga verzija fix
